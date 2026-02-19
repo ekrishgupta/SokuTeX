27d7231ffda5fcb8dcfd1024cba57131a3d072bf
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-18 20:02:24 -0800

    feat: update compiler to support VFS-based compilation

--- src/compiler.rs
+++ src/compiler.rs
@@ -7,10 +7,10 @@ impl Compiler {
         Self
     }
 
-    pub fn compile(latex: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    pub fn compile(latex: &str, _vfs: &crate::vfs::Vfs) -> Result<Vec<u8>, Box<dyn Error>> {
         // Stub: Tectonic removed due to build issues.
-        // In real impl, this would call Tectonic.
-        println!("Compiling: {}", latex);
+        // In real impl, this would use Tectonic with VFS.
+        println!("Compiling from VFS: {}", latex);
         Err("Tectonic compilation disabled".into())
     }
 }
